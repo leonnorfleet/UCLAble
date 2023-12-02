af76013a173e1b247a581d5406b889a9d504b292
Eavan Jael Soriano <eavanjaelsoriano@gmail.com>
2023-12-02 15:09:47 -0800

    Styling to NavBar and Home, renamed main.js to index.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
-import ThemeToggle from './ThemeToggle.js';
+import ThemeToggle from './ThemeToggle.js'
+import '../styles/home.css';
 
 function Home() {
     return (
-        <>
+        <div className="home-container">
         <h1>UCLAble</h1>
         <h2>Development Build</h2>
 
         <ThemeToggle/>
-        </>
+        </div>
     )
 }
 
