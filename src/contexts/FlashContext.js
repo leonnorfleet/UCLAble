c10c1f0a27bb5bb5d5b28bbd41e660399e670f4d
Naman Modani <modani.naman@gmail.com>
2023-12-02 12:58:17 -0800

    Implement FlashProvider

---
+++ src/contexts/FlashContext.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, createContext } from "react";
+
+const FlashContext = createContext();
+
+export function FlashProvider({ children }) {
+  const [flashData, setFlashData] = useState({ show: false, msg: "", success: false });
+
+  const flash = (newFlashData) => {
+    setFlashData({ ...newFlashData });
+  };
+
+  useEffect(() => {
+    let timeout;
+    const { show } = flashData;
+
+    if (show) {
+      timeout = setTimeout(() => {
+        setFlashData({ show: false, msg: "", success: false });
+      }, 4800);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [flashData.show]);
+
+  const contextValue = { flashData, flash };
+
+  return <FlashContext.Provider value={contextValue}>{children}</FlashContext.Provider>;
+}
+
+export default FlashContext;
