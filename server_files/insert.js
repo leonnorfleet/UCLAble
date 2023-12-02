c94bc1638aae67200f633754cb7387ce206cc832
Sunehra <sunehra0714@gmail.com>
2023-12-01 16:19:23 -0800

    Add routes and tester logic for signup and login features on insert.js

--- server_files/insert.js
+++ server_files/insert.js
@@ -12,6 +12,9 @@ const uri = 'mongodb+srv://user:etT59jHnbIxskq2U@cluster0.laxg9wh.mongodb.net/?r
 
 const client = new MongoClient(uri);
 
+//Password hashing
+const bcrypt = require('bcrypt');
+
 /*
 This js script is for uploading javascript objects to the mongodb database in json form
 */
@@ -59,6 +62,68 @@ async function run() {
       res.json(data);
       uploadData(data, users);
     })
+
+    // Add Signup Route
+    app.post('/signup', async (req, res) => {
+      try {
+        // Validate input, hash password, store new user in database
+        const { email, password } = req.body;
+
+        // Validate Email
+        if (!/@(g\.)?ucla\.edu$/.test(email)) {
+          return res.status(400).send("Invalid email. UCLA members only.");
+        }
+
+       // Validate Password
+       if (!/(?=.*[A-Z])(?=.*[@$%!^&#]).{8,}/.test(password)) {
+        return res.status(400).send("Password must have at least eight characters, at least one uppercase letter, and at least one of the special characters: "@", "$", "%", "!", "^", "&", or "#".");
+        }
+
+        // Check if user already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res.status(400).send("User already exists.");
+        }
+
+        // Hash Password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create new user
+        const user = new User({ email, password: hashedPassword });
+        await user.save();
+
+        res.status(201).send("User created successfully.");
+      } catch (error) {
+        res.status(500).send("Error during signup");
+      }
+    });
+
+    // Add Login Route
+    app.post('/login', async (req, res) => {
+      try {
+       // Validate input, verify user, issue token if applicable
+       const { email, password } = req.body;
+
+        // Validate Email and Password Format
+        if (!/@(g\.)?ucla\.edu$/.test(email) || !/(?=.*[A-Z])(?=.*[@$%!^&#]).{8,}/.test(password)) {
+          return res.status(400).send("Invalid email or password format.");
+        }
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+          return res.status(401).send("User not found.");
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return res.status(401).send("Invalid password.");
+        }
+      } catch (error) {
+        res.status(500).send("Error during login");
+      }
+    });
   } finally {
     // Close the MongoDB client connection
     //await client.close();
