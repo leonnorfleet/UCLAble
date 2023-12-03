6b9e184b76d2089e397a96b1bb55e89002f65fc7
Eavan Jael Soriano <eavanjaelsoriano@gmail.com>
2023-12-02 19:09:59 -0800

    Style change to NavBar and Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ import '../styles/home.css';
 function Home() {
     return (
         <div className="home-container">
+        <h2>Welcome to</h2>
         <h1>UCLAble</h1>
-        <h2>Development Build</h2>
 
         <ThemeToggle/>
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom"
-import '../styles/navbar.css';
+import '../styles/navbar.css'
+import logo from '../imgs/uclable_logo.png';
 
 export default function Navbar() {
     return (
         <nav className="nav">
+            <div class="nav-logo">
+                <img src={logo} alt="UCLAble Logo"/>
+            </div>
+             <ul>
             <Link to="/">Home</Link>
             <Link to="/make-a-report">Make a Report</Link>
             <Link to="/see-reports">View Reports</Link>
-            <ul>
             </ul>
         </nav>
     )
