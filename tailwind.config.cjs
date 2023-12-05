100ea33b69dd96c4b4e7a790e652cdd55b665c55
Eavan Jael Soriano <eavanjaelsoriano@gmail.com>
2023-12-04 19:38:33 -0800

    Dark Mode Colors

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    themeToggle: 'class',
+    theme: {
+        extend: {},
+        colors: {
+            secondary: '#dae0e6',
+            primary: '#ffffff',
+            darkText: '#00387A',
+            lightText: '#787c7e',
+            lightRed: '#FCA5A5',
+            brightRed: '#EF4444',
+            brightGreen: '#22c35e',
+            gray: '#3e4446'
+        },
+        fontFamily: {
+            primary: ['Helvetica', 'sans-serif'],
+        },
+    },
+    plugins: [],
+};
