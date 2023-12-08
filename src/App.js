33c814b15cbbfdcfde63a734ddfba09040fd5ff1
leonnorfleet <l.cnor752@gmail.com>
2023-12-07 19:48:45 -0800

    fixed package.json error

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ function App() {
 
           axios.post('http://localhost:8080/account-interact', profileinf)
           .then(res => {
-            //console.log(res.data);
+            console.log(res.data);
           })
           .catch(err => {
             console.log(err);
