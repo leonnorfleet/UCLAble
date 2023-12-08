7ec03d708e9570bc900ef24f839b06bd26d61630
leonnorfleet <l.cnor752@gmail.com>
2023-12-07 21:56:29 -0800

    forgor null check

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 function Profile(props) {
   return (
-    <div>
+    <>
+    {props.profile ? (<div>
       <h2>User Profile</h2>
       <p>Name: {props.profile.name}</p>
       <p>Email: {props.profile.email}</p>
       <p>Liked Posts: {props.likedPostsCount}</p>
-    </div>
+    </div>) : (
+      <h2>Loading...</h2>
+    )}
+    </>
   );
 }
 
