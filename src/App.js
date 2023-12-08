cbf85bc348bc9b3111d70df54ee507663978174b
Eavan Jael Soriano <eavanjaelsoriano@gmail.com>
2023-12-07 23:16:13 -0800

    Feat: Added sign in button to home page

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ function App() {
         <Route path='/make-a-report' element={<Post profile={profile}/>}></Route>
         <Route path='see-reports' element={<View profile={profile}/>}></Route>
         <Route path='/profile' element={<Profile profile={profile} likes={likedPostsCount} />}></Route>
-        <Route path='/' element={<Home profile={profile}/>}></Route>
+        <Route path='/' element={<Home profile={profile} button={abutton}/>}></Route>
       </Routes>
     </div>
     {/*profile ? (
