92cc9b60f80a9a123d1be0be621f9b3ec23e62e8
leon.norfleet <l.cnor752@gmail.com>
2023-12-07 22:18:09 -0800

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,8 @@ function Profile(props) {
       <h2>User Profile</h2>
       <p>Name: {props.profile.name}</p>
       <p>Email: {props.profile.email}</p>
-      <p>Liked Posts: {props.likedPostsCount}</p>
+      <p>Liked Posts: {props.likes}</p>
+      <p>Total Number of Posts: WIP</p>
     </div>) : (
       <h2>Loading...</h2>
     )}
